e6f5ebf710aa16b6ab95fb3abf7a6b8a5687638d
mm44x365 <maxtkj@outlook.com>
2022-08-23 22:55:45 +0700

    maning maning

--- index.php
+++ index.php
@@ -229,7 +229,7 @@
                         Putri Terakhir dari Pasangan <br>
                         <b>Bapak Asmin &amp; Ibu Ismiyati</b>
                     </p>
-                    <p class="m-0">Jl. Irian Jaya Gang Semampir No.143 Rt.06 Rw.01 Desa Banjaran, Kec.Taman, Kab.Pemalang, Jawa Tengah.</p>
+                    <p class="m-0">Jl. Irian Jaya Gang Semampir No.143 Rt.06 Rw.01 Desa Banjaran, Kec.Taman, Kab.Pemalang, Jawa Tengah</p>
                 </div>
                 <div data-aos="fade-down-right" data-aos-easing="ease-out-cubic" data-aos-delay="100" data-aos-duration="1000" class="col-lg-6 text-center couples ">
                     <!-- <div class="circle-image">
@@ -243,7 +243,7 @@
                         Putra Pertama dari Pasangan <br>
                         <b>Bapak Kiswanto &amp; Ibu Agnes Sugiati</b>
                     </p>
-                    <p class="m-0">Jl. Makam Pahlawan No.46 Rt.01 Rw.03 Desa Sungapan, Kec.Pemalang, Kab.Pemalang, Jawa Tengah.</p>
+                    <p class="m-0">Jl. Makam Pahlawan No.46 Rt.01 Rw.03 Desa Sungapan, Kec.Pemalang, Kab.Pemalang, Jawa Tengah</p>
                 </div>
             </div>
         </div>
@@ -264,7 +264,7 @@
                             </p>
                             <p class="desc-location">
                                 <b>Rumah Mempelai Wanita</b> <br>
-                                Jl. Irian Jaya Gang Semampir No.143 Rt.06 Rw.01 Desa Banjaran, Kec.Taman, Kab.Pemalang, Jawa Tengah.
+                                Jl. Irian Jaya Gang Semampir No.143 Rt.06 Rw.01 Desa Banjaran, Kec.Taman, Kab.Pemalang, Jawa Tengah
                             </p>
                         </div>
                     </div>
@@ -280,7 +280,7 @@
                             </p>
                             <p class="desc-location">
                                 <b>Rumah Mempelai Wanita</b> <br>
-                                Jl. Irian Jaya Gang Semampir No.143 Rt.06 Rw.01 Desa Banjaran, Kec.Taman, Kab.Pemalang, Jawa Tengah.
+                                Jl. Irian Jaya Gang Semampir No.143 Rt.06 Rw.01 Desa Banjaran, Kec.Taman, Kab.Pemalang, Jawa Tengah
                             </p>
                         </div>
                     </div>
@@ -568,7 +568,7 @@
                         <div class="col-8">
                             <b>ALAMAT </b><br>
                             a/n Fatikhatul Ilma <br>
-                            Jl. Irian Jaya, Gang Semampir, Desa Banjaran Rt.06, Rw.01, No.143, Kec.Taman, Kab.Pemalang, Jawa Tengah
+                            Jl. Irian Jaya Gang Semampir No.143 Rt.06 Rw.01 Desa Banjaran, Kec.Taman, Kab.Pemalang, Jawa Tengah
                         </div>
                         <div class="col-4">
                             <img src="images/payment/alamat.jpg" alt="Bank " style="width:100%;margin-top:25px">
